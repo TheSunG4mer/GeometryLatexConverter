86c9e051b3c0d2b4b1a0548ec5b1c2a5b5fdad7c
Magnus Merrild <201906074@post.au.dk>
2024-07-18 17:18:59 +0200

    Snapshot: Optimized circle-circle intersection

--- Objects/ConstructionStrategies/PointAsCircleCircleIntersectionConstruction.py
+++ Objects/ConstructionStrategies/PointAsCircleCircleIntersectionConstruction.py
@@ -14,7 +14,22 @@ class PointAsCircleCircleIntersectionConstruction(ConstructionStrategy):
         assert isinstance(circle1, Circle)
         assert isinstance(circle2, Circle)
         assert number in [0, 1]
+        
+        C1, C2 = circle1.getCenter(), circle2.getCenter()
+        x1, y1 = C1.getCoordinates()
+        x2, y2 = C2.getCoordinates()
+        r1, r2 = circle1.getRadius(), circle2.getRadius()
+        d = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+        D = (r1 ** 2 - r2 ** 2 + d ** 2) / (2 * d)
+        orthoLength = (r1 ** 2 - D ** 2) ** 0.5
 
-        radicalAxis = Line(definingObjects=[circle1, circle2], constructionStrategy=RadicalAxisConstruction())
 
-        return PointIntersectionOfLineAndCircleConstruction().constructObject([radicalAxis, circle1, number])
+        deltax = (x2 - x1) / d
+        deltay = (y2 - y1) / d
+
+        newNumber = 2 * number - 1
+
+
+        return x1 + D * deltax + newNumber * orthoLength * deltay, \
+            y1 + D * deltay - newNumber * orthoLength * deltax
+        #PointIntersectionOfLineAndCircleConstruction().constructObject([radicalAxis, circle1, number])
